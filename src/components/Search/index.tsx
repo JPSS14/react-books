a4dbfc3484c71d8262223ad979c61ce263842cda
JPSS14 <jp_scarabelli@hotmail.com>
2021-05-20 02:33:55 -0300

    UPDATE 7: Added style to search component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,19 +1,31 @@
 import { useBooks } from '../../contexts/BooksContext';
 import autocomplete from '../../../autocomplete.json';
+import { AiOutlineSearch, AiFillStar } from 'react-icons/ai';
 import style from './Search.module.scss';
 
 export function Search() {
-    const {newBook, search} = useBooks();
+    const { newBook, search } = useBooks();
     return (
         <div className={style.search}>
+
+            <button className={style.favoriteButton} onClick={search}>
+                <span className={style.buttonText}>Favoritos</span>
+                <AiFillStar className={style.buttonIconLeft} />
+            </button>
+
             <input list="teste" onChange={newBook} type="text" placeholder="digite um livro" />
-            <datalist id="teste">
+            <datalist id="teste" className={style.teste}>
                 {autocomplete.map((item, key) =>
                     item.nome ?
-                        <option value={item.nome} key={key}></option> : <></>
+                        <option value={item.nome} className={style.teste} key={key}></option> : <></>
                 )}
             </datalist>
-            <button onClick={search}>Pesquisar</button>
+
+            <button onClick={search}>
+                <AiOutlineSearch className={style.buttonIconRight} />
+                <span className={style.buttonText}>Pesquisar</span>
+                </button>
+                
         </div>
     );
 }
